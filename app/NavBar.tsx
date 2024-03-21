5a5915b6c7b4fbf927986792562f8b4353c1cb87
Greg Mattia <gjmattia@gmail.com>
2024-03-21 18:38:19 -0400

    added sign out

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,22 @@
+'use client'
 import React from 'react'
 import Link from "next/link";
+import { useSession } from 'next-auth/react';
+import Loading from './loading';
 
 export const NavBar = () => {
+    const { status, data: session } = useSession();
+
+
     return (
         <div className='flex bg-slate-300 p-5 space-x-3'>
             <Link href='/' className='mr-5'>Next.js Project</Link>
             <Link href='/users'>Users</Link>
-            <Link href='/api/auth/signin'>Login</Link>
+            {status === 'loading' && <Loading />}
+            {status === 'authenticated' && <div>{session.user!.name}
+                <Link className='ml-5' href='/api/auth/signout'>Sign Out</Link>
+            </div>}
+            {status === 'unauthenticated' && <Link href='/api/auth/signin'>Login</Link>}
         </div>
     )
 }
